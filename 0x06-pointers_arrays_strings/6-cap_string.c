fde7d516a2f64a192eb7f3d817b88b42217eebea
GoddayOkos <goddayokos@gmail.com>
2022-07-14 22:02:17 -0700

    Capitalize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,33 +12,30 @@ char *cap_string(char *str)
 {
 	int i, j;
 	char separators[] = ",;.!?(){}\n\t\" ";
-	bool to_cap = false;
+	int to_cap = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[0] > 96 && str[0] < 123)
-			to_cap = true;
+			to_cap = 1;
 
 		for (j = 0; separators[j] != '\0'; j++)
 		{
 			if (separators[j] == str[i])
-				to_cap = true;
+				to_cap = 1;
 		}
 
 		if (to_cap)
 		{
-			if (to_cap)
+			if (str[i] > 96 && str[i] < 123)
 			{
-				if (str[i] > 96 && str[i] < 123)
-				{
-					str[i] -= 32;
-					to_cap = false;
-				}
-				else if (str[i] > 64 && str[i] < 91)
-					to_cap = false;
-				else if (str[i] > 47 && str[i] < 58)
-					to_cap = false;
+				str[i] -= 32;
+				to_cap = 0;
 			}
+			else if (str[i] > 64 && str[i] < 91)
+				to_cap = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				to_cap = 0;
 		}
 	}
 
