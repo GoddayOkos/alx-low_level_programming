77b6fb6766dd63051dfdacb4d605902db108ed71
GoddayOkos <goddayokos@gmail.com>
2022-07-14 20:19:55 -0700

    Compare two Strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,12 @@
 
 int _strcomp(char *s1, char *s2)
 {
-	int i;
+	int j;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (j = 0; s1[j] != '\0' || s2[j] != '\0'; j++)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (s1[j] != s2[j])
+			return (s1[j] - s2[j]);
 	}
 
 	return (0);
