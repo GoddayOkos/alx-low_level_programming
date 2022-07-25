6da4dc1822272fafc5f7d718187f6283467a0413
GoddayOkos <goddayokos@gmail.com>
2022-07-25 00:59:00 -0700

    Change coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A proogram that prints the minimum number of coins to make for amount
+ * @argc: The arguements' counter
+ * @argv: The argument's values
+ * Return: 0 as Success or 1 for Error.
+ */
+
+int main(int argc, char *argv[])
+{
+	int amount, coins = 0;
+
+	if (argc == 2)
+	{
+		amount = atoi(argv[1]);
+
+		if (amount < 0)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		if (amount % 1 >= 0)
+			coins += amount;
+		if (amount % 2 >= 0)
+		{
+			coins += amount / 2;
+			amount %= 2;
+		}
+		if (amount % 5 >= 0)
+		{
+			coins += amount / 5;
+			amount %= 5;
+		}
+		if (amount % 10 >= 0)
+		{
+			coins += amount / 10;
+			amount %= 10;
+		}
+		if (amount % 25 >= 0)
+		{
+			coins += amount / 25;
+			amount %= 25;
+		}
+
+		printf("%d\n", coins);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
