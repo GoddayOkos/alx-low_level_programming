773840bf1ce7d212c9a5490563992390f645aa6a
GoddayOkos <goddayokos@gmail.com>
2022-08-04 04:32:01 -0700

    Goals

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,12 +12,18 @@
 
 int main(int argc, char *argv[])
 {
-	int (*func)(int, int) = get_op_func(argv[2]);
+	int (*func)(int, int);
 
-	if (argc != 4 || func == NULL)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(98);
+	}
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
 	}
 
 	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
