0f7e0b8b2a809a0aacb6b941b898467e2968bbb5
GoddayOkos <goddayokos@gmail.com>
2022-08-05 01:51:25 -0700

    Fixed errors

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(ap, int);
+		str = va_arg(ap, char*);
 		if (str == NULL)
 			printf("(nil)");
 		else
