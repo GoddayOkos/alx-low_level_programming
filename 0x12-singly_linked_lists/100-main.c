fb11f8afe5995aeb5b7848b0e3098a12c8df4f5c
GoddayOkos <goddayokos@gmail.com>
2022-08-12 20:18:52 -0700

    Fixing first

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 as Success
+ */
+
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature,
+	challenges one to a race.)\n");
+
+	return (0);
+}
