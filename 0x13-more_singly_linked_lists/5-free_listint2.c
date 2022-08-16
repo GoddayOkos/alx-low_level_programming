c3013fbfc1f2fbbc9d1b2c13f168401ed3aebf49
GoddayOkos <goddayokos@gmail.com>
2022-08-16 14:39:40 -0700

    Debugging free2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	while (*head)
@@ -18,6 +18,5 @@ void free_listint2(listint_t **head)
 		*head = *head->next;
 		free(temp);
 	}
-	free(*head);
 	*head = NULL;
 }
