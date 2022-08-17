d1f9413085391f5e01cc4d5ccf4f3456194bbbdc
GoddayOkos <goddayokos@gmail.com>
2022-08-17 06:07:35 -0700

    Debugging reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,14 +13,14 @@ listint_t *reverse_listint(listint_t **head)
 	if (*head == NULL || head == NULL)
 		return (NULL);
 
-	while (*head->next)
+	while ((*head)->next != NULL)
 	{
-		next = *head->next;
-		*head->next = prev;
+		next = (*head)->next;
+		(*head)->next = prev;
 		prev = *head;
 		*head = next;
 	}
-	*head->next = prev;
+	(*head)->next = prev;
 
 	return (*head);
 }
